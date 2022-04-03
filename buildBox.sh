6f340780e8aee4e04a9ef97d9960038addaec99e
Kenny Miltenberger <klmiltenbergerjr@gmail.com>
2022-04-03 16:06:37 -0400

    Update buildBox.sh
    added pwntools cli support

--- buildBox.sh
+++ buildBox.sh
@@ -63,6 +63,7 @@ sudo apt install ghidra
 sudo apt install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pwntools
+echo "export PATH=$PATH:~/.local/bin" >> $homeDir/.zshrc
 
 # install impacket
 mkdir -p $myToolDir/impacket
