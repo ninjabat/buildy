5adb90b19af3009e3215993840ab451600c7ef2b
Kenny Miltenberger <klmiltenbergerjr@gmail.com>
2023-11-16 11:38:47 -0800

    Update 000-UpdateSystem.sh

--- scripts/000-UpdateSystem.sh
+++ scripts/000-UpdateSystem.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# make it so services restart automatically when updates (will allow automatic updating)
+sudo sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf
+
 echo "updating operating system..."
 sudo apt update
 sudo apt upgrade -y
